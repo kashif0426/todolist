7216be899f454f42a1f6cd62dd02a47e1b788c80
kashif0426 <82613468+kashif0426@users.noreply.github.com>
2022-10-29 12:24:49 +0200

    This is a to do list

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-const grocerices = document.getElementsByClassName("groceries")[0];
+const groceries = document.getElementsByClassName("groceries")[0];
 const eraser = document.getElementById("eraser");
-const allIteam = document.getElementById("allIteam");
-const userInput = document.getElementById("userInput")
+const allItems = document.getElementById("allIteam");
+const userInput = document.getElementById("userInput");
 
 eraser.addEventListener("click", function() {
-    allIteam.innerHTML = '';
+    allItems.innerHTML = "";
 })
 
 userInput.addEventListener("keydown", function(event) {
-    if (event.key == Enter)
+    if (event.key == "Enter")
         addIteam();
 })
 
 function addIteam() {
-    var h2 = document.createElement('h2');
+    var h2 = document.createElement("h2");
     h2.innerHTML = "- " + userInput.value;
 
     h2.addEventListener("click", function() {
         h2.style.textDecoration = "line-through";
-
-        allIteam.insertAdjacentElement("beforeend", h2);
-
-
     })
+
+    allItems.insertAdjacentElement("beforeend", h2);
+
+    userInput.value = "";
 }
